e84646376e33e4c36b7aad25ee44217dac843960
nikhilchandra80 <chandranikhil80@gmail.com>
2017-03-03 06:11:55 -0800

    adding code

---
+++ complete/src/main/java/hello/DataAccess.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class DataAccess {
+	
+	public final int sno;
+	public final String name;
+	public final int value;
+	
+	 public int getSno() {
+	        return sno;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+	    
+	    public int getValue(){
+	    	return value;
+	    	
+	    }
+
+}
